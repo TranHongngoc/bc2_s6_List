1db9176fb1c7d9e83131fc8b051ff27d73c5edaa
Tran Hong Ngoc <hongngoctran0@gmail.com>
2018-11-12 08:11:20 +0700

    add

---
+++ src/MyListTest.java
@@ -0,0 +1,10 @@
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> listInterger = new MyList<Integer>();
+        listInterger.add(1);
+        listInterger.add(2);
+
+        System.out.println("element 2: " + listInterger.get(1));
+        System.out.println("element 1: " + listInterger.get(0));
+    }
+}
